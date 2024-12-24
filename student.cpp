7442ae828035a67bb08e159b2e068ddbcfdb8dbb
Rony Azad <154042218+RonyAzad012@users.noreply.github.com>
2024-12-24 15:30:18 +0600

    add cgp

--- student.cpp
+++ student.cpp
@@ -1,32 +1,30 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 class Student
 {
 public:
-int id;
-int age;
-string name;
+    int id;
+    int age;
+    float cgp;
+    string name;
 
- Student ( int i , int a, string n)
- {
-    id = i;
-    age = a;
-    name = n;
- }
- void show()
- {
-     cout<< "student Id:"<< id<< endl;
-    cout<<"Student Age:"<<age<<endl;
-    cout<<"Student Name: "<< name<< endl;
-
- }
-
-
+    Student(int i, int a, string n, float cp)
+    {
+        id = i;
+        cgp = cp;
+        age = a;
+        name = n;
+    }
+    void show()
+    {
+        cout << "student Id:" << id << endl;
+        cout << "Student Age:" << age << endl;
+        cout << "Student Name: " << name << endl;
+    }
 };
 int main()
 {
-    Student S1 (23, 3323,"Mumu");
+    Student S1(23, 3323, "Mumu");
     S1.show();
 }
-
