42081d4a0839417220f2aee763fa32d4d82f92f8
Anika Bushra Rumpa <anikabushrarumpa4566@gmail.com>
2025-01-03 23:23:43 +0600

    adding main Function

--- student.cpp
+++ student.cpp
@@ -98,4 +98,36 @@ void searchStudent()
     }
     cout << "Student with ID " << searchId << " not found.\n";
 }
+// Main function
+int main() {
+    int choice;
 
+    do {
+        cout << "\nStudent Management System\n";
+        cout << "1. Add Student\n";
+        cout << "2. View Students\n";
+        cout << "3. Search Student\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            addStudent();
+            break;
+        case 2:
+            viewStudents();
+            break;
+        case 3:
+            searchStudent();
+            break;
+        case 4:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice! Please try again.\n";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
